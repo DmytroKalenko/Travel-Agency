3ea85544fc7ac65151fbe8f071c4ceec3ef3b804
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-01-25 15:05:19 +0100

    add slider

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
+
 $(document).ready(function(){
     $(".slider").slick({
-     slidesToShow:5,   
+        slidesToShow:6,   
      speed:1500,
-     autoplay: true,
+     autoplay: false,
      autoplaySpeed: 1000,  
-     centerMode: true,   
-
+     centerMode: true, 
     });
-
-
-
 });
