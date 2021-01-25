549085bd9765cb0256439a090c172401285c73d1
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-01-26 00:28:15 +0100

    new design for slider. Slider is working))

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $(".slider").slick({
         slidesToShow:6,   
      speed:1500,
-     autoplay: false,
+     autoplay: true,
      autoplaySpeed: 1000,  
      centerMode: true, 
     });
