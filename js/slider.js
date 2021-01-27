0804ea97f0cb35e25db7d4254faf5c1191b0689b
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-01-27 23:49:15 +0100

    add puups

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".slider2").slick({
+     slidesToShow:1,     
+     speed:1200,
+     autoplay: false,
+     autoplaySpeed: 1000,   
+     waitForAnimate: false
+    });
+});
