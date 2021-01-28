c939088e575bb558a9b0a6c126ee3c1ce5c6c02c
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-01-28 20:17:32 +0100

    add media filec , slider fo pop-up

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     $(".slider2").slick({
-     slidesToShow:1,     
-     speed:1200,
-     autoplay: false,
-     autoplaySpeed: 1000,   
-     waitForAnimate: false
+     slidesToShow:1,  
+     speed:2000,
+     autoplay: true,
+     autoplaySpeed: 1200, 
     });
 });
