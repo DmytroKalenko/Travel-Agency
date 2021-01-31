55eaac8dac87ab6e3df0764e3c47fb1df445e477
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-01-31 13:01:07 +0100

    adaptive slider

--- js/script.js
+++ js/script.js
@@ -7,6 +7,51 @@ $(document).ready(function(){
      autoplaySpeed: 1000,  
      centerMode: true,
      focusOnSelect: true, 
-     waitForAnimate: false
+     waitForAnimate: false,
+
+
+
+     responsive: [
+        {
+            breakpoint: 2500,
+            settings: {
+              slidesToShow: 7,
+            }
+          },
+
+          {
+            breakpoint: 1439,
+            settings: {
+              slidesToShow: 5,
+            }
+          },  
+        
+        {
+          breakpoint: 1023,
+          settings: {
+            slidesToShow: 5,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+          
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+         
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
+      mobileFirst:true
+
+
     });
 });
