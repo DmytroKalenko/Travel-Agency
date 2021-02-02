b9762e7c2d6c38fa72994127e1c30a688a36e08a
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-02-02 14:27:59 +0100

    adaptive pop-up

--- js/slider.js
+++ js/slider.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
      autoplay: true,
      autoplaySpeed: 1200, 
      varibleWidth: true,
+    responsive:[
+        {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              arrows:false,
+              dots:true,
+            }
+          },
+    ]
     });
 });
