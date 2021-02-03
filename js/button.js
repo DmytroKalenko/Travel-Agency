c9efdb2c9496c478aedb90b88651f4cb042dc6ff
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-02-03 11:16:13 +0100

    add buttons and it programing

--- js/button.js
+++ js/button.js
@@ -1,3 +1,7 @@
-$("buttonid").click(function () {
-    $("divid").toggle();
-    });
+function openForm() {
+    document.getElementById("background-popup").style.display = "flex";
+  }
+
+  function closeForm() {
+    document.getElementById("background-popup").style.display = "none";
+  }
