1891b8b66676affeae10e3ad6a4cd18af27eba77
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-02-05 10:08:51 +0100

    new adaptive  new buttoms

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,47 @@
 $(document).ready(function(){
     $(".slider2").slick({
-     slidesToShow:2,  
+     slidesToShow:3,  
      speed:2000,
      autoplay: true,
      autoplaySpeed: 1200, 
      varibleWidth: true,
     responsive:[
         {
-            breakpoint: 767,
+            breakpoint: 480,
             settings: {
               slidesToShow: 1,
               arrows:false,
               dots:true,
             }
           },
+
+          {
+            breakpoint: 967,
+            settings: {
+              slidesToShow: 2,
+              arrows:false,
+              dots:true,
+            }
+          },
+
+          {
+            breakpoint: 1025,
+            settings: {
+              slidesToShow: 3,
+              arrows:false,
+              dots:true,
+            }
+          },
+
+          {
+            breakpoint: 1441,
+            settings: {
+              slidesToShow: 4,
+              arrows:false,
+              dots:true,
+            }
+          },
+
     ]
     });
 });
