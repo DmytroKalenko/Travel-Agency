6ec3b1fbc3915be5c4cc0899479bf59508deb64a
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-02-06 09:51:00 +0100

    new life

--- js/button.js
+++ js/button.js
@@ -2,6 +2,10 @@ function openForm() {
     document.getElementById("background-popup").style.display = "flex";
   }
 
+ 
+
   function closeForm() {
     document.getElementById("background-popup").style.display = "none";
   }
+
+
