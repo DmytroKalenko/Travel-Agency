e1182deb14644eeedbf39937e26e38b96f5283c6
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-02-07 17:03:57 +0100

    slider

--- js/script.js
+++ js/script.js
@@ -2,12 +2,9 @@
 $(document).ready(function(){
     $(".slider").slick({
      slidesToShow:7,   
-     speed:1200,
-     autoplay: true,
-     autoplaySpeed: 1000,  
      centerMode: true,
      focusOnSelect: true, 
-     waitForAnimate: false,
+     waitForAnimate: true,
      responsive: [
         {
             breakpoint: 2500,
